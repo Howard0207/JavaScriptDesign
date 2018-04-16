b12b4e1c952a3a9bbdddfe2e30b003af67e758b9
Howard0207 <wode163_youjian@163.com>
2018-04-16 15:24:34 +0800

    format

--- nextTick.js
+++ nextTick.js
@@ -1,29 +1,29 @@
 window.onload = function () {
-    $('.btn').click(function () {
-        domReady(abc);
-        $('.chat').append('<div class="test">HelloWorld</div>');
-        $('.test').append('<div class="age">World</div>');
-    });
+  $('.btn').click(function () {
+    domReady(abc);
+    $('.chat').append('<div class="test">HelloWorld</div>');
+    $('.test').append('<div class="age">World</div>');
+  });
 
-    function abc() {
-        console.log("dom改变了");
-    }
+  function abc() {
+    console.log("dom改变了");
+  }
 };
 
 
 let domReady = function (callback, article, options, times) {
-    article = article || document.body;
-    options = options || {'childList': true, 'attributes': true, 'characterData': true, 'subtree': true};
-    times = !times;
-    let execute = function () {
-        callback();
-        if (times) {
-            // 清除历史监听记录
-            observer.takeRecords();
-            // 断开连接
-            observer.disconnect();
-        }
-    };
-    let observer = new MutationObserver(execute);
-    observer.observe(article, options);
+  article = article || document.body;
+  options = options || {'childList': true, 'attributes': true, 'characterData': true, 'subtree': true};
+  times = !times;
+  let execute = function () {
+    callback();
+    if (times) {
+      // 清除历史监听记录
+      observer.takeRecords();
+      // 断开连接
+      observer.disconnect();
+    }
+  };
+  let observer = new MutationObserver(execute);
+  observer.observe(article, options);
 };
